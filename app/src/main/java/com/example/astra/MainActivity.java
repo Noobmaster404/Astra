1773bd4905969ecee10801b3022f1c77723a53aa
Erik <erikeksuzan058@gmail.com>
2025-03-23 15:09:09 +0300

    test

--- app/src/main/java/com/example/astra/MainActivity.java
+++ app/src/main/java/com/example/astra/MainActivity.java
@@ -10,7 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
-
+//Test
 public class MainActivity extends AppCompatActivity {
 
     @Override
