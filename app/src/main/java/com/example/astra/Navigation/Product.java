f9c51c2c1389dad7a647d2bae30f424678dd310b
Erik <erikeksuzan058@gmail.com>
2025-03-28 11:28:43 +0300

    Astra bug fixed 1.23.1

--- app/src/main/java/com/example/astra/Navigation/Product.java
+++ app/src/main/java/com/example/astra/Navigation/Product.java
@@ -2,12 +2,12 @@ package com.example.astra.Navigation;
 //
 public class Product {
     private String name;
-    private double price;
+    private String price;
     private String imageUrl;
 
     public Product() {} // Пустой конструктор для Firebase
 
-    public Product(String name, double price, String imageUrl) {
+    public Product(String name, String price, String imageUrl) {
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
@@ -17,8 +17,8 @@ public class Product {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
-    public double getPrice() { return price; }
-    public void setPrice(double price) { this.price = price; }
+    public String getPrice() { return price; }
+    public void setPrice(String price) { this.price = price; }
 
     public String getImageUrl() { return imageUrl; }
     public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
